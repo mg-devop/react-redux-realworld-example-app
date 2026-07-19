9d97d8f969f244756d9a31401324111c0b2ccc25
mg-devop <gopidevops2025@gmail.com>
2026-07-19 14:42:22 +0530

    Update ArticlePreview.js

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -37,7 +37,11 @@ const ArticlePreview = props => {
     <div className="article-preview">
       <div className="article-meta">
         <Link to={`/@${article.author.username}`}>
-          <img src={article.author.image} alt={article.author.username} />
+          <img
+            src={article.author.image || `https://ui-avatars.com/api/?name=${article.author.username}&size=64&background=random`}
+            style={{ width: '32px', height: '32px', borderRadius: '50%', objectFit: 'cover' }}
+            alt={article.author.username}
+          />
         </Link>
 
         <div className="info">
